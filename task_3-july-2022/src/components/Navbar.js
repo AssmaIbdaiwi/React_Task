6e4a46cf5894f51db12ec0ba54acfb42134c1685
AssmaIbdaiwi <100359495+AssmaIbdaiwi@users.noreply.github.com>
2022-07-04 09:32:36 +0300

    main

---
+++ task_3-july-2022/src/components/Navbar.js
@@ -0,0 +1,41 @@
+import React, {Component} from 'react';
+import {ThemeContext} from '../contexts/ThemeContext';
+import {AuthContext} from '../contexts/AuthContext';
+
+class Navbar extends Component {
+    render() { 
+      
+
+
+        return (
+          <AuthContext.Consumer>{(AuthContext)=>(
+      
+        <ThemeContext.Consumer>{(themeContext)=>{
+          const {isAuthenticated, toggleAuth  } = AuthContext;
+          const{ isLightTheme, light, dark}= themeContext;
+          const theme = isLightTheme ? light : dark;
+          return(
+            <nav  style={{background:theme.ui, color:theme.syntax}}>
+            <h1> Navbar </h1>
+            <div onClick={toggleAuth }>
+              {isAuthenticated ? 'Logged in' : 'Logged out'}
+            </div>
+        <ul >
+          <li > Home  </li>
+          <li > About </li>
+          <li > Contact </li> 
+        </ul>
+    
+    </nav>
+          )
+        }}
+
+</ThemeContext.Consumer>
+)}</AuthContext.Consumer>
+
+
+        );
+    }
+}
+ 
+export default Navbar;
